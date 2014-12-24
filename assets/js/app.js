7d37baf4a8031da1f206b960c4ec9f4c157fc147
usualdesigner <usualdesigner@gmail.com>
2014-12-24 14:10:03 +0200

    exrience basic page

---
+++ assets/js/app.js
@@ -0,0 +1,54 @@
+var mySite = angular.module('mySite', ['ui.router']);
+var ExperienceCtrl = function () {
+    var vm = this;
+    vm.list = [
+        {
+            company: 'Bilberry',
+            position: 'Php developer',
+            range: '2013-2014'
+        },
+        {
+            company: 'Bilberry',
+            position: 'Php developer',
+            range: '2013-2014'
+        },
+        {
+            company: 'Bilberry',
+            position: 'Php developer',
+            range: '2013-2014'
+        },
+        {
+            company: 'Bilberry',
+            position: 'Php developer',
+            range: '2013-2014'
+        },
+        {
+            company: 'Bilberry',
+            position: 'Php developer',
+            range: '2013-2014'
+        },
+        {
+            company: 'Bilberry',
+            position: 'Php developer',
+            range: '2013-2014'
+        },
+    ];
+}
+mySite.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/home");
+    $stateProvider
+        .state('home', {
+            url: "/home",
+            templateUrl: "templates/home.html"
+        })
+        .state('experience', {
+            url: "/experience",
+            templateUrl: "templates/experience.html",
+            controller: ExperienceCtrl,
+            controllerAs: 'experience'
+        })
+        .state('contact', {
+            url: "/contact",
+            templateUrl: "templates/contact.html"
+        });
+});
